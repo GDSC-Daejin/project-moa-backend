9efe98bef545ebee51ae20fe90e0740bff8ff112
myeongju00 <j51677767@gmail.com>
2023-11-04 15:50:25 +0900

    [feat] : 불필요한 import문 제작

--- src/main/java/com/gdsc/moa/domain/user/controller/AuthController.java
+++ src/main/java/com/gdsc/moa/domain/user/controller/AuthController.java
@@ -8,16 +8,11 @@ import com.gdsc.moa.domain.user.service.AuthService;
 import com.gdsc.moa.global.jwt.oauth.UserInfo;
 import com.gdsc.moa.global.message.UserMessage;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
