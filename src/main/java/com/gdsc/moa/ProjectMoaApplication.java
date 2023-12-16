d033f9825dcce5a1be4a6aea0c078aeadcf7ee82
jeonsangmin <jsangmin99@gmail.com>
2023-12-16 17:30:04 +0900

    [feat] : 스웨거 https 설정

--- src/main/java/com/gdsc/moa/ProjectMoaApplication.java
+++ src/main/java/com/gdsc/moa/ProjectMoaApplication.java
@@ -1,9 +1,12 @@
 package com.gdsc.moa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://moa-backend.kro.kr", description = "Moa 프로젝트 API 명세서")})
 @SpringBootApplication
 @EnableScheduling
 public class ProjectMoaApplication {
